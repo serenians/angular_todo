fd3b8b2e2b6f1ebeb7052e1e4f639ce8811dd5be
Sagar Khyaju <skhyaju@deerwalk.com>
2019-06-17 14:55:41 +0545

    bootstrap working"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from '../app/controller/about/about.component'
+import { HomeComponent } from '../app/controller/home/home.component'
+import { ContactComponent } from '../app/controller/contact/contact.component'
+import { fromEventPattern } from 'rxjs';
+import { AppComponent } from './app.component';
+const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'about', component: AboutComponent},
+{path: 'contact', component: ContactComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './controller/about/about.component';
+import { ContactComponent } from './controller/contact/contact.component';
+import { NavComponent } from './controller/nav/nav.component';
+import { HomeComponent } from './controller/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
