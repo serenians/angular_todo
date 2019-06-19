fb11070e544df97344de391525cf520b8fb84bdf
Sagar Khyaju <skhyaju@deerwalk.com>
2019-06-19 11:40:33 +0545

    firebase package intalled + imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,7 @@ import { HomeComponent } from './controller/home/home.component';
 import { TodoComponent } from './controller/todo/todo.component';
 import { NotFoundComponent } from './controller/not-found/not-found.component';
 import { LocalStorageService } from './services/storage/local-storage.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,8 +27,10 @@ import { LocalStorageService } from './services/storage/local-storage.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule,,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [
     LocalStorageService
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCKhRGLwRdKo6zbdgCD3k3K0pL2GKxrfrc",
+    authDomain: "todo-sample-4ade8.firebaseapp.com",
+    databaseURL: "https://todo-sample-4ade8.firebaseio.com",
+    projectId: "todo-sample-4ade8",
+    storageBucket: "todo-sample-4ade8.appspot.com",
+    messagingSenderId: "477073620518",
+    appId: "1:477073620518:web:c261dbd8fd618b52"
+  }
 };
